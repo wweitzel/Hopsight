88b2f8e5a25a25b9e81b7d5b2c687a193b883bef
Wes Weitzel <wweitzel@omitron.com>
2019-08-09 20:35:25 -0400

    Add camera input

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  async handleImage(event) {
+    const file = event.target.files[0]; 
+    const base64 = await this.toBase64(file);
+  }
+
+  private toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
 
 }
