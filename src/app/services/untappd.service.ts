54308f389c643f894e81c2e9b96be7532c6866aa
Wes Weitzel <wweitzel@omitron.com>
2019-08-10 10:31:20 -0400

    Externalize api keys. Setup urls and api keys in environment variable.

--- src/app/services/untappd.service.ts
+++ src/app/services/untappd.service.ts
@@ -2,29 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const CLIENT_ID = 'client_id';
-const CLIENT_SECRET = 'client_secret';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UntappdService {
 
+  private url = environment.untappd_url;
+  private clientId = environment.untappd_client_id;
+  private clientSecret = environment.untappd_client_secret;
+
   constructor(private http: HttpClient) { }
 
   getBeerInfo(beerId: number): Observable<any> {
-    const url = 'https://api.untappd.com/v4/beer/info/' + beerId;
+    const url = this.url + '/beer/info/' + beerId;
     let params = new HttpParams();
-    params = params.append('client_id', CLIENT_ID);
-    params = params.append('client_secret', CLIENT_SECRET);
+    params = params.append('client_id', this.clientId);
+    params = params.append('client_secret', this.clientSecret);
     return this.http.get(url, { params });
   }
 
   getSearchResults(text: string) {
-    const url = 'https://api.untappd.com/v4/search/beer'
+    const url = this.url + '/search/beer';
     let params = new HttpParams();
-    params = params.append('client_id', CLIENT_ID);
-    params = params.append('client_secret', CLIENT_SECRET);
+    params = params.append('client_id', this.clientId);
+    params = params.append('client_secret', this.clientSecret);
     params = params.append('q', text);
     return this.http.get(url, { params });
   }
