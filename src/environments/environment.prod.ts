4fd43cce26e6d7f0844f527cd5fed2fd5e5ade73
Wes Weitzel <wweitzel@omitron.com>
2019-09-13 17:50:46 -0400

    Add default configuration for firebase hosting of PWA

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+import { api_keys } from '../../api_keys/keys';
+
 export const environment = {
-  production: true
+  production: true,
+
+  ocr_space_url: 'https://api.ocr.space',
+  untappd_url: 'https://api.untappd.com/v4',
+
+  untappd_client_id: api_keys.UNTAPPD_CLIENT_ID,
+  untappd_client_secret: api_keys.UNTAPPED_CLIENT_SECRET,
+  ocr_space_api_key: api_keys.OCR_SPACE_API_KEY
 };
